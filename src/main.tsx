7ca5dda72d4c52f5efeacfa43394906d0b1ba136
创圣大天使Evol <296662402@qq.com>
2024-07-24 15:48:22 +0800

    integrate with eslint && prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,12 @@
-import { ThemeProvider } from '@emotion/react'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import theme from './theme'
-import { JotaiProvider } from './providers/jotai-provider'
 import ColorModeProvider from './providers/color-mode-provider'
+import { JotaiProvider } from './providers/jotai-provider'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        {/* <ThemeProvider theme={theme}>
-            <JotaiProvider>
-                <App />
-            </JotaiProvider>
-        </ThemeProvider> */}
         <ColorModeProvider>
             <JotaiProvider>
                 <App />
